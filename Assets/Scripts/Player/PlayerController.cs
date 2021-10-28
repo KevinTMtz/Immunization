da6aa9c5f01831bf1c5ef763f461466250a048b8
Kevin Torres Martínez <torresmtzkevin@gmail.com>
2021-10-28 13:27:43 -0500

    Character movement with controller (#14)
    * Imported Creator Kit: FPS assets
    
    * Player movement with controller
    
    * Fixed room colliders & Player prefab path
    
    * Remove collisions between players

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -10,8 +10,16 @@ public class PlayerController : MonoBehaviourPunCallbacks, IPunObservable
 
     public Transform cameraTransform;
 
+    public float playerSpeed = 5.0f;
+    public float speed = 0.0f;
+    float verticalSpeed = 0.0f;
+
+    CharacterController characterController;
+
     void Start()
     {
+        characterController = GetComponent<CharacterController>();
+
         if (!photonView.IsMine)
         {
             GetComponentInChildren<Camera>().enabled = false;
@@ -23,7 +31,25 @@ public class PlayerController : MonoBehaviourPunCallbacks, IPunObservable
     {
         if (photonView.IsMine)
         {
-            // TODO: Add player and camera movement
+            speed = 0;
+            Vector3 movement = Vector3.zero;
+
+            movement = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
+            if (movement.sqrMagnitude > 1.0f)
+                movement.Normalize();
+
+            movement = movement * playerSpeed * Time.deltaTime;
+
+            movement = transform.TransformDirection(movement);
+            characterController.Move(movement);
+
+            verticalSpeed = verticalSpeed - 10.0f * Time.deltaTime;
+            if (verticalSpeed < -10.0f) verticalSpeed = -10.0f;
+            
+            var verticalMove = new Vector3(0, verticalSpeed * Time.deltaTime, 0);
+            var flag = characterController.Move(verticalMove);
+            
+            if ((flag & CollisionFlags.Below) != 0) verticalSpeed = 0;
         }
     }
 
