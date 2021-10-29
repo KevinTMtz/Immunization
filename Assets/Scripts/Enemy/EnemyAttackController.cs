7126e0fa0fe68d5b4efa15dd76b4e2275b6fd802
KevinTMtz <torresmtzkevin@gmail.com>
2021-10-29 02:48:39 -0500

    Enemies shoot player

--- Assets/Scripts/Enemy/EnemyAttackController.cs
+++ Assets/Scripts/Enemy/EnemyAttackController.cs
@@ -5,14 +5,41 @@ using UnityEngine;
 public class EnemyAttackController : MonoBehaviour
 {
     GameObject player;
+
+    public GameObject bullet;
+    public Transform shootPoint;
+
+    private float shootWaitTime;
+
+    private bool ableToShoot;
+    private float startTime;
+    private float endTime;
     
     void Start()
     {
+        shootWaitTime = 1f;
         player = GameObject.Find("Player(Clone)");
     }
 
     void Update()
     {
-        
+        if (Vector3.Distance(player.transform.position, gameObject.transform.position) < 10)
+        {
+            gameObject.transform.LookAt(player.transform.position);
+
+            if (Time.time > endTime && ableToShoot == false)
+                ableToShoot = true;
+
+            if (ableToShoot) {
+                GameObject bulletInstantiated = Instantiate(bullet, shootPoint.position, shootPoint.rotation);
+
+                Rigidbody bulletRB = bulletInstantiated.GetComponent<Rigidbody>();
+                bulletRB.AddForce(shootPoint.right * 10, ForceMode.Impulse);
+
+                startTime = Time.time;
+                endTime = startTime + shootWaitTime;
+                ableToShoot = false;
+            }
+        }
     }
 }
